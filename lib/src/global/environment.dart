44440642a8385356a034678f173ada38fd3d0601
gestionarlaweb <gestionarlaweb@gmail.com>
2021-08-03 23:24:41 +0200

    a ver ahora4

--- lib/src/global/environment.dart
+++ lib/src/global/environment.dart
@@ -1,5 +1,4 @@
 class Environment {
-  static String apiKey = '';
   static String baseUrl = 'productos-edc12-default-rtdb.firebaseio.com';
   static String baseUrlGoogle = 'identitytoolkit.googleapis.com';
   static String firebaseToken = 'AIzaSyCjDhCjKkAJQpfs0tKfYNvuTPZVRlxtj54';
